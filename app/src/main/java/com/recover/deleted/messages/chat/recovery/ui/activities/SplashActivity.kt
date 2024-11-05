e6b812c07b628588a3c45b3d2552e9fd95134588
devzain-star <dev.zain711@gmail.com>
2024-11-05 13:08:38 +0500

    Onboarding

--- app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/SplashActivity.kt
@@ -5,11 +5,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.recover.deleted.messages.chat.recovery.base.BaseActivity
+import com.recover.deleted.messages.chat.recovery.databinding.ActivityMainBinding
+import com.recover.deleted.messages.chat.recovery.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
+    lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         installSplashScreen()
 
         startActivity(Intent(this, OnboardingActivity::class.java))

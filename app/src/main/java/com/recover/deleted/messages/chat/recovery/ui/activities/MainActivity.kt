398050e8874ceee5f8e7caeed7292dd133163976
devzain-star <dev.zain711@gmail.com>
2024-11-11 19:54:12 +0500

    Home Screen

--- app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.recover.deleted.messages.chat.recovery.R
+import com.recover.deleted.messages.chat.recovery.base.BaseActivity
 import com.recover.deleted.messages.chat.recovery.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +25,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        init()
+    }
+
+    fun init(){
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(calendar.time)
+
+        // Format day as "Monday"
+        val dayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+        val formattedDay = dayFormat.format(calendar.time)
+
+        binding.date.text = formattedDate
+        binding.day.text = formattedDay
     }
 }

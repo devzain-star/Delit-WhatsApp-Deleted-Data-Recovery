24003de355650ecd8cbc9e67ee9c51190874e448
devzain-star <dev.zain711@gmail.com>
2024-11-13 14:11:54 +0500

    Quick Send

--- app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/QuickSendActivity.kt
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/QuickSendActivity.kt
@@ -1,6 +1,9 @@
 package com.recover.deleted.messages.chat.recovery.ui.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,10 +26,33 @@ class QuickSendActivity : BaseActivity() {
         }
         init()
 
+        binding.sendBtn.setOnClickListener {
+            val phoneNumber = binding.phoneEt.text.toString()
+            val message = binding.bodyEt.text.toString()
+
+            if (phoneNumber.isNotEmpty() && message.isNotEmpty()) {
+                sendMessage(phoneNumber, message)
+            } else {
+                Toast.makeText(this, "Please enter both phone number and message", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     fun init(){
         setHeader("Quick Send")
+    }
 
+    private fun sendMessage(phoneNumber: String, message: String) {
+        try {
+            val formattedNumber = if (phoneNumber.startsWith("+")) phoneNumber else "+$phoneNumber"
+            val uri = Uri.parse("https://api.whatsapp.com/send?phone=$formattedNumber&text=$message")
+
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, "WhatsApp not installed or other error occurred", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
     }
 }

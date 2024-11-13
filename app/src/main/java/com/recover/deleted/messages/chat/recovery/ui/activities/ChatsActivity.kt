6cac1b0718789cbbcd54968b951723257f770863
devzain-star <dev.zain711@gmail.com>
2024-11-13 12:50:51 +0500

    Quick Send

--- app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/ChatsActivity.kt
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/ChatsActivity.kt
@@ -1,17 +1,26 @@
 package com.recover.deleted.messages.chat.recovery.ui.activities
 
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.recover.deleted.messages.chat.recovery.R
+import com.recover.deleted.messages.chat.recovery.base.BaseActivity
+import com.recover.deleted.messages.chat.recovery.databinding.ActivityChatsBinding
 
-class ChatsActivity : AppCompatActivity() {
+class ChatsActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityChatsBinding
+    private lateinit var emptyLay: RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chats)
+        binding = ActivityChatsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        emptyLay = binding.root.findViewById(R.id.emptyLayout)
+        setHeader(getString(R.string.chats))
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/ImagesActivity.kt
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/ImagesActivity.kt
@@ -1,17 +1,26 @@
 package com.recover.deleted.messages.chat.recovery.ui.activities
 
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.recover.deleted.messages.chat.recovery.R
+import com.recover.deleted.messages.chat.recovery.base.BaseActivity
+import com.recover.deleted.messages.chat.recovery.databinding.ActivityImagesBinding
 
-class ImagesActivity : AppCompatActivity() {
+class ImagesActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityImagesBinding
+    private lateinit var emptyLay: RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_images)
+        binding = ActivityImagesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        emptyLay = binding.root.findViewById(R.id.emptyLayout)
+        setHeader(getString(R.string.images))
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

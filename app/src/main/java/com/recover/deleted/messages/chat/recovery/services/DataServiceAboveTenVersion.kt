235d99343ed5b1ad9cf15d9794c09b5ff7081e67
devzain-star <dev.zain711@gmail.com>
2024-11-15 16:38:29 +0500

    Status Savour

---
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/services/DataServiceAboveTenVersion.kt
@@ -0,0 +1,11 @@
+package com.recover.deleted.messages.chat.recovery.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class DataServiceAboveTenVersion : Service(){
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}

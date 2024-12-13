24cd55e29f978d77723e9b51ed9ab63e06189f8f
devzain-star <dev.zain711@gmail.com>
2024-12-13 23:17:26 +0500

    Working on Images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.perf)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/recover/deleted/messages/chat/recovery/ui/activities/SettingActivity.kt
@@ -0,0 +1,93 @@
+package com.recover.deleted.messages.chat.recovery.ui.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.recover.deleted.messages.chat.recovery.R
+import com.recover.deleted.messages.chat.recovery.base.BaseActivity
+import com.recover.deleted.messages.chat.recovery.databinding.ActivitySettingBinding
+
+
+class SettingActivity : BaseActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+    var versionName = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setInsets()
+        setHeader("Settings")
+
+        binding.contactCard.setOnClickListener {
+            contactUs()
+        }
+
+        binding.shareCard.setOnClickListener {
+            shareApp()
+        }
+
+        binding.rateCard.setOnClickListener {
+            rateUs()
+        }
+
+
+    }
+
+    fun setInsets(){
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun contactUs() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("support@yourapp.com"))
+            putExtra(Intent.EXTRA_SUBJECT, "Contact Us: Your App Name")
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "No email app found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun shareApp() {
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            val appPackageName = packageName
+            val appLink = "https://play.google.com/store/apps/details?id=$appPackageName"
+            putExtra(Intent.EXTRA_TEXT, "Check out this amazing app: $appLink")
+        }
+
+        startActivity(Intent.createChooser(shareIntent, "Share App via"))
+    }
+
+    fun rateUs() {
+        val appPackageName = packageName
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=$appPackageName")
+                )
+            )
+        } catch (e: android.content.ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")
+                )
+            )
+        }
+    }
+}
